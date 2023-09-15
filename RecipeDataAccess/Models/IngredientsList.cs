4396af894902c219dc60a74219a1dab1c7c2bfff
Rolleksy <aleksanderpurcha@gmail.com>
2023-09-15 13:45:43 +0200

    razor component now displays recipes selected from dropdown

---
+++ RecipeDataAccess/Models/IngredientsList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeDataAccess.Models
+{
+    public class IngredientsList
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string QuantityType { get; set; }
+    }
+}
